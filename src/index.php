988e8021cb0ad8bdbd2108ea8f0f412899d6caf2
R0kkxSynetique <60880661+R0kkxSynetique@users.noreply.github.com>
2021-04-29 13:06:17 +0200

    create low basic code

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file index.php
+ * @brief This is the router, it handles requests and redirect them to the correct controller
+ * @author Created by Benjamin.Fontana@cpnv.ch
+ * @version 0.1 / 29.04.2021
+ */
+
+// Creates/Resumes a sessions
+session_start();
+
+// Dependencies
+require_once "conrtoller/nav.php";
+
+// Routing
+switch (strtok($_SERVER["REQUEST_URI"], '?')) {
+    
+    default:
+        lost();
+
+}
---
+++ src/view/template.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file template.php
+ * @brief Centralizes all common graphical components like header and footer
+ * @author Created by Benjamin.Fontana@cpnv.ch
+ * @version 0.1 / 29.04.2021
+ */
