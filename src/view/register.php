f66ca16eb46dbe4deffb5fa12c42f8a8034b1a26
NathanaelCollaud <61788141+NathanaelCollaud@users.noreply.github.com>
2021-05-17 14:57:44 +0200

    Update register.php

--- src/view/register.php
+++ src/view/register.php
@@ -8,49 +8,49 @@ function registerView()
 
     ?>
 
-    <div class="container " >
+    <div class="container" >
         <br>
         <h4 align="center" >S'inscrire</h4><br>
-<div class="border">
+<div class=" justify-content-center">
         <form  action="index.php?action=login" method="post">
-            <div class="row d-flex justify-content-center">
-                <div class="col-sm-2">
+            <div class="row ">
+                <div class="col-2">
                     <label for="inputNewEmailAddress">E-Mail</label>
                     <input type="email" name="inputNewEmailAddress" id="inputNewEmailAddress">
                 </div>
 
-                <div class="col-sm-2">
+                <div class="col-2">
                     <label for="inputNewPsw">Mot de passe</label><br>
                     <input  type="password" name="inputNewPsw" id="inputNewPsw">
                 </div>
             </div><br>
 
-            <div class="row d-flex justify-content-center">
-                <div class="col-sm-2">
+            <div class="row">
+                <div class="col-2">
                     <label for="inputNewFirstName" >Prénom</label>
                     <input type="text" name="inputNewFirstName" id="inputNewFirstName">
                 </div>
 
-                <div class="col-sm-2">
+                <div class="col-2">
                     <label for="inputConfirmPsw">Confirmer mot de passe</label><br>
                     <input type="password" name="inputConfirmPsw" id="inputConfirmPsw">
                 </div>
             </div><br>
-            <div class="row d-flex justify-content-center">
-                <div class="col-sm-2">
+            <div class="row">
+                <div class="col-2">
                     <label for="inputNewLastName">Nom</label><br>
                     <input type="text" name="inputNewlastName" id="inputNewlastName">
                 </div >
-                <div class="col-sm-2">
+                <div class="col-2">
                     <label for="inputNewBirthDate">Date de naissance</label><br>
                     <input type="date" name="inputNewBirthDate" id="inputNewBirthDate">
                 </div>
             </div><br>
-            <div class="row d-flex justify-content-center">
-                <div class="col-sm-2 text-end">
+            <div class="row">
+                <div class="col-2 text-end">
                 <input type="reset" value="Effacer">
                 </div>
-                <div class="col-sm-2">
+                <div class="col-2">
                     <input type="submit" value="S'inscrire">
                 </div>
             </div>
