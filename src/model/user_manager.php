0b757fd2084cd58f89e6c3e9ad67b2379df0c5ff
MUMINOVIC Benjamin <Benjamin.MUMINOVIC@cpnv.ch>
2021-05-17 14:53:24 +0200

    Added user login function and new client code generation

--- src/model/user_manager.php
+++ src/model/user_manager.php
@@ -1,26 +1,45 @@
 <?php
+/**
+ * @author : Benjamin Muminovic
+ * @description : Contains all the functions to manage users in the database
+ *
+ */
 
 require "model/dbConnector.php";
 
 /**
+ * @return string The new code for the user
+ * @Description Generates a new code for an user based on the last generated code
+ */
+function generateId() {
+    $query = "SELECT * FROM `luxhub`.`people`;";
+    $result = executeQuerySelect($query);
+
+    $lastRegister = end($result);
+    $lastId =  (int)$lastRegister[2];
+
+    $newId = $lastId++;
+    return (string)$newId;
+}
+
+/**
  * @param array $registerData
  * @description Registers an user in the database with the information given
- * @return  bool
+ * @return bool True if the execution worked, false otherwise
  */
 function registerUser($registerData) {
     // By default the account type should be a member
     $accountType = 1;
-    // Temporary solution to generate an unique id for the client
-    $clientCode = uniqid();
-    //Hashes the password
+    // Generates a new code for the client
+    $clientCode = generateId();
+    // Hashes the password
     $password = password_hash($registerData["password"], PASSWORD_DEFAULT);
+
 
     //TODO change the name of registerData's array names in the query
     $lastname = $registerData['lastname'];
     $firstname = $registerData['firstname'];
     $email = $registerData['email'];
-
-    // Date format for database "YYYY-MM-DD"
     $birthdate = $registerData['birthdate'];
 
     $query = "INSERT INTO `luxhub`.`people` (`Account_type_id`, `client_code`, `lastname`, `firstname`, `email`, `password`, `birthdate`) VALUES ($accountType, $clientCode, $lastname, $firstname, $email, $password, $birthdate);";
@@ -30,7 +49,7 @@ function registerUser($registerData) {
 
 /**
  * @param $userEmail
- * @return bool Returns true if the user already exists, false in the opposite case.
+ * @return bool Returns true if the user already exists, false otherwise.
  */
 function verifyUser($userEmail) {
     $query = "SELECT `email` FROM `people` WHERE `email` = $userEmail;";
@@ -43,4 +62,21 @@ function verifyUser($userEmail) {
     }
 
     return $success;
+}
+
+function loginUser($loginData) {
+    $userMail = $loginData['email'];
+    $query = "SELECT `password` FROM `people` WHERE `email` = $userMail;";
+    $result = executeQuerySelect($query);
+
+    $pswVerif = password_verify($loginData['password'], $result[0][0]);
+
+    if($pswVerif) {
+        $success = true;
+    } else {
+        $success = false;
+    }
+
+    return $success;
+
 }
