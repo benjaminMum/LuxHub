1ee2310189529b265ceed93400833fd901f555e2
Enbun9000 <61788752+Enbun9000@users.noreply.github.com>
2021-06-06 10:09:08 +0200

    Got the user page in shape

--- src/view/user.php
+++ src/view/user.php
@@ -8,16 +8,21 @@ function userView($uData)
     ob_start();
 
 ?>
+    <link rel="stylesheet" href="view/css/user.css">
 
-    <h2>User page</h2>
+<div class="container pt-5 align-content-center">
+    <h2 class="text-center">Votre compte</h2>
 
-    <button><a href="/modifyUser">Modifier</a></button>
+    <div class="pt-4 mx-auto col-4 text-center">
+        <p class="name-style"><?= $uData['firstname']?> <?= $uData['lastname']?></p>
+        <p>E-Mail : <?= $uData['email']?></p>
 
-    <p><?= $uData['email']?></p>
-    <p><?= $uData['firstname']?></p>
-    <p><?= $uData['lastname']?></p>
-    <p><?= $uData['birthdate']?></p>
+        <p>Date de naissance : <?= $uData['birthdate']?></p>
+        <button class="btn btn-secondary text-center"><a href="/modifyUser" class="text-white">Modifier</a></button>
+    </div>
 
+
+</div>
 
 <?php
 
